21b5d04f8d00e378caaabde3270c02c4c8340ad6
Mykhailo Hulii <miha080104@gmail.com>
2025-02-24 15:39:35 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo pacman -S git ranger htop inxi acpi neovim zip unzip telegram-desktop translate-shell zsh xclip ttf-jetbrains-mono-nerd docker docker-compose ttf-jetbrains-mono-nerd curl nodejs npm openssh jdk-openjdk
+sudo pacman -S git ranger htop inxi acpi neovim zip unzip telegram-desktop translate-shell zsh xclip ttf-jetbrains-mono-nerd docker docker-compose ttf-jetbrains-mono-nerd curl nodejs npm openssh jdk-openjdk dotnet-runtime dotnet-sdk aspnet-runtime
 
 # setting the docker
 sudo groupadd docker
@@ -41,5 +41,10 @@ mkdir -p ~/Documents/Mark/Programming/Android
 cd ~/Documents/Mark/Programming
 git clone https://github.com/Miha0809/AutoSelect.git
 git clone https://github.com/Miha0809/react-pizza.git
+
+# setting oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 echo "INSTALL A .NET"
